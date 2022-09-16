bbab62f83ca731b2b8e90116da2f6597d4972e65
Vaibhav Gupta <vaibhavgupta6394709250@gmail.com>
2022-09-16 11:28:12 +0530

    created a new app

---
+++ CRUD/Models/employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Salary { get; set; }
+    }
+    
+}
